3f8dece75617cd209f9d466d2e18dc34d9027e86
SuNgu <cliffland.jasungu@gmail.com>
2024-03-01 00:54:14 +0300

    corrected parentElement

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,12 +3,12 @@ function showContent(element){
     const serviceContents = document.getElementsByClassName('service-content');
 
     for (let i = 0; i < serviceImages.length; i++){
-        serviceImages[i].classList.remove('active');
+        serviceImages[i].parentElement.classList.remove('active');
         serviceContents[i].classList.remove('active');
     }
 
-    const currentServiceImage = element.parentElement;
-    const currentServiceContent = currentServiceImage.nextElementSibling;
+    const currentService = element.parentElement;
+    //const currentServiceContent = currentServiceImage.nextElementSibling;
 
     currentServiceImage.classList.add('active');
     currentServiceContent.classList.add('active');
