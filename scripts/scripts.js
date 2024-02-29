1b9dd3af3b42c2434ea410e4b0ffc35ffaee5016
SuNgu <cliffland.jasungu@gmail.com>
2024-03-01 01:07:41 +0300

    added new function to js to enable show and hide functions

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,29 @@
-function showContent(element){
-    const serviceImages = document.getElementsByClassName('service-image');
-    const serviceContents = document.getElementsByClassName('service-content');
+// function showContent(element){
+//     const serviceImages = document.getElementsByClassName('service-image');
+//     const serviceContents = document.getElementsByClassName('service-content');
 
-    for (let i = 0; i < serviceImages.length; i++){
-        serviceImages[i].parentElement.classList.remove('active');
-        serviceContents[i].classList.remove('active');
+//     for (let i = 0; i < serviceImages.length; i++){
+//         serviceImages[i].parentElement.classList.remove('active');
+//         serviceContents[i].classList.remove('active');
+//     }
+
+//     const currentService = element.parentElement;
+//     //const currentServiceContent = currentServiceImage.nextElementSibling;
+
+//     currentService.classList.add('active');
+//     currentService.nextElementSibling.classList.add('active');
+// }
+
+function showContent(event) {
+    const services = document.getElementsByClassName('service');
+  
+    for (let i = 0; i < services.length; i++) {
+      const serviceContent = services[i].getElementsByClassName('service-content')[0];
+      services[i].classList.remove('active');
+      serviceContent.classList.remove('active');
     }
 
-    const currentService = element.parentElement;
-    //const currentServiceContent = currentServiceImage.nextElementSibling;
-
-    currentServiceImage.classList.add('active');
-    currentServiceContent.classList.add('active');
+    const currentService = event.target.parentElement;
+    currentService.classList.add('active');
+    currentService.nextElementSibling.classList.add('active');
 }
